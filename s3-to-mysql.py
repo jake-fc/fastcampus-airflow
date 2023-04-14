9921520f17023e1b8bc93e391234bfbc8a0238a8
jake-fc <128557896+jake-fc@users.noreply.github.com>
2023-04-14 17:27:56 +0900

    Create s3-to-mysql.py

---
+++ s3-to-mysql.py
@@ -0,0 +1,32 @@
+from airflow import DAG
+from airflow.providers.mysql.operators.mysql import MySqlOperator
+from airflow.providers.amazon.aws.operators.s3_to_mysql import S3ToMySqlOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 4, 12),
+    'retry_delay': timedelta(minutes=10),
+}
+
+
+
+dag = DAG(
+    's3_to_rds_mysql',
+    default_args=default_args,
+    schedule_interval=timedelta(days=1),
+)
+
+
+s3_to_mysql_task = S3ToMySqlOperator(
+    task_id='s3_to_mysql',
+    s3_bucket='jake-api',
+    s3_key='api/upbit-api/year={{ ds_nodash[:4] }}/month={{ ds_nodash[4:6] }}/day={{ ds_nodash[6:8] }}/hour={{ ds_nodash[8:10] }}/data.txt',
+    mysql_conn_id='mysql_conn',
+    table='upbit_api',
+    dag=dag,
+)
+
+s3_to_mysql_task
+
