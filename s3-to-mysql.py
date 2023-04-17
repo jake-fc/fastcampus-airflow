429beb549f345f27c1b0907fc78a7d7b8ce75d67
jake-fc <128557896+jake-fc@users.noreply.github.com>
2023-04-17 10:11:12 +0900

    Update s3-to-mysql.py

--- s3-to-mysql.py
+++ s3-to-mysql.py
@@ -21,6 +21,7 @@ dag = DAG(
 
 s3_to_mysql_task = S3ToMySqlOperator(
     task_id='s3_to_mysql',
+    aws_conn_id='aws_default',
     s3_source_key='jake-api/api/upbit-api/year={{ ds_nodash[:4] }}/month={{ ds_nodash[4:6] }}/day={{ ds_nodash[6:8] }}/hour={{ ds_nodash[8:10] }}/data.txt',
     mysql_conn_id='mysql_conn',
     mysql_table='upbit_api',
