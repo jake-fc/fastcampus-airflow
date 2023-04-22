5354f3ec8471427b8e64ec69a0ed8dfe1120bf2a
jake-fc <128557896+jake-fc@users.noreply.github.com>
2023-04-22 20:35:06 +0900

    Update s3hook_to_mysql.py

--- s3hook_to_mysql.py
+++ s3hook_to_mysql.py
@@ -4,13 +4,9 @@ from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.providers.mysql.operators.mysql import MySqlOperator
 from airflow.providers.mysql.hooks.mysql import MySqlHook
 from airflow.operators.python_operator import PythonOperator
-
-from datetime import datetime, timedelta
-
 import pandas as pd
 import io
 import json
-
 default_args = {
     'owner': 'jake',
     'depends_on_past': False,
@@ -18,58 +14,43 @@ default_args = {
     'retries': 0,
     'retry_delay': timedelta(minutes=5),
 }
-
 dag = DAG(
     's3_to_mysql_v2',
     default_args=default_args,
-    schedule_interval='30 * * * *',
-    catchup=True
+    schedule_interval=timedelta(days=1),
 )
-
 s3_bucket_name = 'jake-api'
 mysql_table_name = 'upbit_api'
 mysql_conn_id = 'mysql_conn'
 s3_conn_id = 'aws_default'
 
-    
-def insert_s3_data_bulk(execution_date, **context):
-    ds_nodash = execution_date.strftime('%Y%m%d%H')
+def insert_s3_data_bulk(**context):
     s3_hook = S3Hook(aws_conn_id=s3_conn_id)
-    s3_prefix = f'api/upbit-api/year={ds_nodash[:4]}/month={ds_nodash[4:6]}/day={ds_nodash[6:8]}/hour={ds_nodash[8:10]}/'
+    s3_files = s3_hook.list_keys(s3_bucket_name)
+    
     s3_files = s3_hook.list_keys(bucket_name=s3_bucket_name, prefix = s3_prefix)
-    
-    print(f'Today is {ds_nodash}')
 
-    print(s3_prefix)
-    print(s3_files)
-    
     json_lists = []
     cleaned_s3_files = []
     for s3_file in s3_files :
         if "year=2023" in s3_file:
             cleaned_s3_files.append(s3_file)
-
     print("Done 1")
-
     for cleaned_s3_file in cleaned_s3_files:
         s3_object = s3_hook.get_key(cleaned_s3_file, s3_bucket_name)
         s3_file = s3_object.get()['Body'].read().decode('utf-8')
         json_lists += [json.loads(json_str) for json_str in s3_file.strip().split('\n')]
-
     print("Done 2")    
     
     del_columns = ["s3_bucket","idx_name","s3_path","@version","@timestamp"]
     for json_list in json_lists:
         for del_column in del_columns:
             json_list.pop(del_column)
-
     print("Done 3")
-
     for json_list in json_lists:
         df = pd.DataFrame.from_dict([json_list])
         mysql_hook = MySqlHook(mysql_conn_id=mysql_conn_id)
         mysql_hook.insert_rows(table=mysql_table_name, replace=True, rows=df.values.tolist())
-
 with dag:
     # S3 파일을 MySQL에 적재하는 PythonOperator
     load_s3_file_to_mysql_task = PythonOperator(
@@ -78,3 +59,5 @@ with dag:
         queue="celery",
         provide_context=True
     )
+
+insert_s3_data_bulk
