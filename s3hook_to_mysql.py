31de73488c204a83e657cc6a7559c2000d570e47
jake-fc <128557896+jake-fc@users.noreply.github.com>
2023-04-22 19:39:36 +0900

    Update s3hook_to_mysql.py

--- s3hook_to_mysql.py
+++ s3hook_to_mysql.py
@@ -33,12 +33,12 @@ s3_conn_id = 'aws_default'
 
     
 def insert_s3_data_bulk(**context):
-    ds_nodash = ds.replace('-', '')
+    ds_nodash = execution_date.strftime('%Y%m%d%H')
     s3_hook = S3Hook(aws_conn_id=s3_conn_id)
     s3_prefix = f'api/upbit-api/year={ds_nodash[:4]}/month={ds_nodash[4:6]}/day={ds_nodash[6:8]}/hour={ds_nodash[8:10]}/'
     s3_files = s3_hook.list_keys(bucket_name=s3_bucket_name, prefix = s3_prefix)
     
-    print(f'Today is {ds}, and the execution date is {execution_date}')
+    print(f'Today is {ds_nodash}')
 
     print(s3_prefix)
     print(s3_files)
